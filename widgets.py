122e69f0ee37b40db490140e2784121a3167d54e
syed-afrid <afrid0052@gmail.com>
2025-03-25 22:19:53 +0530

    File Menu

--- widgets.py
+++ widgets.py
@@ -1,16 +1,25 @@
 from tkinter import *
 
+def menu_bar(window):
+    menubar = Menu(window)
+    window.config(menu=menubar)
+    #File Menu
+    fileMenu = Menu(menubar,tearoff=0)
+    menubar.add_cascade(label="File",menu=fileMenu)
+    fileMenu.add_command(label="Open")
+    fileMenu.add_command(label="Save")
+    fileMenu.add_separator()
+    fileMenu.add_command(label="Exit")
+
+
 def set_widgets(window):
+
+    menu_bar(window)
     
     editor = Text(window,
                   bg="light yellow",
                   padx=5,
                   pady=5,
-                  fg="black",height=400,
+                  fg="black",
                   relief=FLAT,
-                  borderwidth=0).pack(side=RIGHT)
-    directory = Listbox(window,
-                        bg="light blue",
-                        height=400,
-                        relief=FLAT,
-                        borderwidth=0).pack(side=LEFT)
+                  borderwidth=0).pack(expand=True,fill='both')
