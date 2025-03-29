edc39df24a822f43c27fb371a1a20f8d5868d69f
syed-afrid <afrid0052@gmail.com>
2025-03-29 21:33:57 +0530

    New file

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 main_window = None
+file_path = None
--- widgets.py
+++ widgets.py
@@ -2,30 +2,38 @@ from tkinter import *
 from tkinter import filedialog
 import config
 
-def open_file():
-    global file_path
-    file_path = filedialog.askopenfile()
-    config.main_window.title(file_path.name)
-    with open(file_path.name) as file:
+def open_file():   
+    config.file_path = filedialog.askopenfile()
+    config.main_window.title(config.file_path.name)
+    with open(config.file_path.name) as file:
         content = file.read()
         editor.insert(1.0,content)
         file.close()
         
 def save_file():
-    with open(file_path.name,'w') as file:
-        content = editor.get(1.0,END)
-        file.write(content)
-        file.close()
+    if config.file_path != None:
+        config.main_window.title(config.file_path.name)
+        with open(config.file_path.name,'w') as file:
+            content = editor.get(1.0,END)
+            file.write(content)
+            file.close()
+    else:
+        save_as_file()
 
 def save_as_file():
-    f = filedialog.asksaveasfile(defaultextension=".txt",filetypes=[('Text',".txt"),('All Files',".*")])
+    config.file_path = filedialog.asksaveasfile(defaultextension=".txt",filetypes=[('Text',".txt"),('All Files',".*")])
     filetext = editor.get(1.0,END)
-    f.write(filetext)
-    f.close()
-    if f is None:
+    config.file_path.write(filetext)
+    config.file_path.close()
+    if config.file_path is None:
         return
     else:
-        config.main_window.title(f.name)
+        config.main_window.title(config.file_path.name)
+
+def new_file():
+    editor.delete(1.0,END)
+    config.main_window.title("Notes")
+    file_path = None
 
 
 def menu_bar(window):
@@ -34,11 +42,18 @@ def menu_bar(window):
     #File Menu
     fileMenu = Menu(menubar,tearoff=0)
     menubar.add_cascade(label="File",menu=fileMenu)
+    fileMenu.add_command(label="New",command=new_file)
     fileMenu.add_command(label="Open",command=open_file)
     fileMenu.add_command(label="Save",command=save_file)
     fileMenu.add_command(label="Save As",command=save_as_file)
     fileMenu.add_separator()
     fileMenu.add_command(label="Exit",command=lambda : config.main_window.destroy())
+    #Edit Menu
+    editMenu = Menu(menubar,tearoff=0)
+    menubar.add_cascade(label="Edit",menu=editMenu)
+    editMenu.add_command(label="Undo")
+    editMenu.add_command(label="Copy")
+    editMenu.add_command(label="Paste")
 
 
 def set_widgets(window):
